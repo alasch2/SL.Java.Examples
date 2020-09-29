55e878e7347f06ac859e697ecb2eeb56c7c5dd16
alasch2 <ala.schneider@sealights.io>
2020-09-29 11:25:49 +0300

    Deleted files, submitted by mistake

--- scripts/sl-test-all.bat
+++
@@ -1,1 +0,0 @@
-call mvn test -P sl-all  -Dsl.build=%1 -pl !sl-example-generate-class
--- scripts/sl-test-some.bat
+++
@@ -1,1 +0,0 @@
-call mvn test -P sl-all -Dsl.build=%1 -pl !sl-example-generate-class,!sl-example-spring-cloud-contract,!spring-cloud-contract-producer,!spring-cloud-contract-consumer
