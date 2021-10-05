62ca362b0a47ca611ae14b0ca51950f4ad3b307e
alasch2 <ala.schneider@sealights.io>
2021-10-05 15:34:59 +0300

    added simple project

---
+++ simple-java-example/src/main/java/io/sl/ex/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package io.sl.ex.calculator;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+}
